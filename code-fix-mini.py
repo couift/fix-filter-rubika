ce8d93c4dd4170e2f7c7fd237fcc40acaee37084
Fish hacker <157135548+couift@users.noreply.github.com>
2024-03-06 14:23:57 +0330

    Update code-fix-mini.py

--- code-fix-mini.py
+++ code-fix-mini.py
@@ -24,7 +24,7 @@ sleep(1)
 print(f"\033[34m")
 
 repoestsrub = ['Rubika.ir/raf-filter','Rubika.ir/fixing-filter','Rubika.ir/Account-liberation','Rubika.ir/rf-fil-rubuk','Rubika.ir/Please-unfilter-this-account','Rubika.ir/plase-fix-filter-this-account','Rubika.ir/fix-fix-fil','Rubika.ir/fixing-fil','Rubika.ir/moslim-account','Rubika.ir/Religious-account','Rubika.ir/Support-fix-filter','Rubika.ir/This-Account-Undertone-Do-Not','Rubika.ir/This-Account-Mistake-Filtered','Rubika.ir/This-Account-must-fix-Filter','Rubika.ir/Bring-this-account-from-the-sanctuary','Rubika.ir/rf-filtering','Rubika.ir/raf-filter','Rubika.ir/fixing-filter','Rubika.ir/Account-liberation','Rubika.ir/rf-fil-rubuk','Rubika.ir/Please-unfilter-this-account','Rubika.ir/plase-fix-filter-this-account','Rubika.ir/fix-fix-fil','Rubika.ir/fixing-fil','Rubika.ir/moslim-account','Rubika.ir/Religious-account','Rubika.ir/Support-fix-filter','Rubika.ir/This-Account-Undertone-Do-Not','Rubika.ir/This-Account-Mistake-Filtered','Rubika.ir/This-Account-must-fix-Filter','Rubika.ir/Bring-this-account-from-the-sanctuary','Rubika.ir/rf-filtering']
-1 = random.choice(repoestsrub)
+1 == random.choice(repoestsrub)
 
 y0y =f"""
 
