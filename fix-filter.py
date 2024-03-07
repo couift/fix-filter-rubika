84da9798082572a95d5f595ea09d8e4d29d09bf1
Fish hacker <157135548+couift@users.noreply.github.com>
2024-03-07 16:08:44 +0330

    Delete fix-filter.py

--- fix-filter.py
+++
@@ -1,5 +0,0 @@
-import random 
- words = ['Rubika.ir/raf-filter','Rubika.ir/fixing-filter','Rubika.ir/Account-liberation','Rubika.ir/rf-fil-rubuk','Rubika.ir/  Please-unfilter-this-account','Rubika.ir/plase-fix-filter-this-account','Rubika.ir/fix-fix-fil','Rubika.ir/fixing-fil','Rubika.  ir/moslim-account','Rubika.ir/Religious-account','Rubika.ir/Support-fix-filter','Rubika.ir/This-Account-Undertone-Do-Not','Rubika.ir/  This-Account-Mistake-Filtered','Rubika.ir/This-Account-must-fix-Filter','Rubika.ir/Bring-this-account-from-the-sanctuary','Rubika.ir/rf-  filtering','Rubika.ir/raf-filter','Rubika.ir/fixing-filter','Rubika.ir/Account-liberation','Rubika.ir/rf-fil-rubuk','Rubika.ir/  Please-unfilter-this-account','Rubika.ir/plase-fix-filter-this-account','Rubika.ir/fix-fix-fil','Rubika.ir/fixing-fil','Rubika.  ir/moslim-account','Rubika.ir/Religious-account','Rubika.ir/Support-fix-filter','Rubika.ir/This-Account-Undertone-Do-Not','Rubika.ir/  This-Account-Mistake-Filtered','Rubika.ir/This-Account-must-fix-Filter','Rubika.ir/Bring-this-account-from-the-sanctuary','Rubika.ir/rf-  filtering']
- input("To get the filter fix code, enter 1: ")
- word = random.choice(words)
- print(word)
